945eed419386a910af9c1b1a7a5d4b5408ba9543
RRZhai <126899363+RRZhai@users.noreply.github.com>
2024-01-25 17:13:38 -0800

    having problem install sql

--- server/app.py
+++ server/app.py
@@ -5,7 +5,11 @@ from flask_restful import Resource
 # Local imports
 from config import app, db, api
 # Add your model imports
-
+from server.models.users import User
+from server.models.lists import List
+from server.models.tasks import Task
+from server.models.achievements import Achievement
+from server.models.follows import Follow
 
 # Views go here!
 
