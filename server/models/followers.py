906c07580d8d2c757747b38ab78cad85789394f1
RRZhai <126899363+RRZhai@users.noreply.github.com>
2024-01-25 16:44:28 -0800

    create followers model

---
+++ server/models/followers.py
@@ -0,0 +1,10 @@
+from __init__ import db, validates, re, validates, relationship
+
+class Follow(db.Model):
+    __tablename__ = 'followers'
+    follower_id = db.Column(db.Integer, db.ForeignKey('users.id'), primary_key=True)
+    followed_id = db.Column(db.Integer, db.ForeignKey('users.id'), primary_key=True)
+    timestamp = db.Column(db.DateTime, nullable=False, default=db.func.now())
+
+    follower = relationship('User', foreign_keys=[follower_id], back_populates='followed_users', lazy='joined')
+    followed = relationship('User', foreign_keys=[followed_id], back_populates='followers', lazy='joined')
--- server/models/users.py
+++ server/models/users.py
@@ -7,9 +7,37 @@ class User(db.Model):
     _password_hash = db.Column(db.String, nullable=False)
     username = db.Column(db.String, nullable=False, unique=False)
     profile_image_url = db.Column(db.String, nullable=True, unique=False)
-    followed = db.relationship('User', secondary='followers', primaryjoin='User.id == Follow.follower_id', secondaryjoin='User.id == Follow.followed_id', back_populates='followers', lazy='dynamic')
+    followed = relationship('User', secondary='followers', primaryjoin='User.id == Follow.follower_id', secondaryjoin='User.id == Follow.followed_id', back_populates='followers', lazy='dynamic')
     tasks = relationship('Task', back_populates='user', cascade='all, delete-orphan')
     achievements = relationship('Achievement', back_populates='user', cascade='all, delete-orphan')
     
     def __repr__(self):
         return f'<User {self.username}>:\n' + f'\tEmail: {self.email}\n' + f'\tProfile Image URL: {self.profile_image_url}\n' + f'\tTasks: {self.tasks}\n' + f'\tAchievements: {self.achievements}\n'
+    
+    @property
+    def password(self):
+        raise AttributeError('Password is not readable.')
+    
+    @password.setter
+    def password(self, password):
+        self._password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self._password_hash, password)
+    
+    @validates('email')
+    def validate_email(self, key, email):
+        if not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+            raise ValueError('Invalid email address.')
+        return email
+    
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.append(user)
+    
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.followed.remove(user)
+    
+    def is_following(self, user):
+        return self.followed.filter(Follow.followed_id == user.id).count() > 0
