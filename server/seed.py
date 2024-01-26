954b7c4c9cb7ea5ec54b3067df150c11ca2e7639
RRZhai <126899363+RRZhai@users.noreply.github.com>
2024-01-25 17:06:36 -0800

    create achievement

--- server/seed.py
+++ server/seed.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 # Standard library imports
 from random import randint, choice as rc
 
@@ -9,6 +7,13 @@ from faker import Faker
 # Local imports
 from app import app
 from server.models.users import db
+
+# Import models here
+from server.models.users import User
+from server.models.lists import List
+from server.models.tasks import Task
+from server.models.achievements import Achievement
+from server.models.follows import Follow
 
 if __name__ == '__main__':
     fake = Faker()
